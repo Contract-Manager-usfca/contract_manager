58cf9045c1010688151ce36cf3e89eeef8669b0d
clarejw <cjwilson6@dons.usfca.edu>
2023-09-22 13:21:10 -0700

    Updated aftre Jags meeting

--- database.sql
+++ database.sql
@@ -1,28 +1,28 @@
-CREATE TABLE creators (
+CREATE TABLE creator (
   id serial PRIMARY KEY,
   name varchar(255) NOT NULL,
   username varchar(255) NOT NULL,
   password varchar(255) NOT NULL
 );
 
-CREATE TABLE platforms (
+CREATE TABLE platform (
   id serial PRIMARY KEY,
   name varchar(255) NOT NULL
 );
 
-INSERT INTO platforms (name)
+INSERT INTO platform (name)
 VALUES
   ('YouTube'),
   ('Instagram'),
   ('TikTok'),
   ('Twitter');
 
-CREATE TABLE demographics (
+CREATE TABLE demographic (
   id serial PRIMARY KEY,
   demographic varchar(255) NOT NULL
 );
 
-INSERT INTO demographics (demographic)
+INSERT INTO demographic (demographic)
 VALUES
   ('Race'),
   ('Gender'),
@@ -32,33 +32,39 @@ VALUES
   ('Language'),
   ('Genre');
 
-# TODO FIX THESE SHITS
-CREATE TABLE contracts (
-  id integer PRIMARY KEY,
-  creator integer,
-  partner varchar(255) NOT NULL,
-  amount_paid integer NOT NULL,
-  date_signed timestamp NOT NULL,
-  FOREIGN KEY (creator) REFERENCES creators (id)
+CREATE TABLE partner (
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL
 );
 
-CREATE TABLE creator_platforms (
+CREATE TABLE contract (
+  id serial PRIMARY KEY,
+  user integer,
+  partner_id integer,
+  amount_paid integer NOT NULL,
+  start_date timestamp NOT NULL,
+  end_date timestamp NOT NULL,
+  FOREIGN KEY (user) REFERENCES creator(id),
+  FOREIGN KEY (partner_id) REFERENCES partner(id)
+);
+
+CREATE TABLE creator_platform (
   creator_id integer,
   platform_id integer,
   follower_count integer NOT NULL,
   handle varchar(255) NOT NULL,
   last_update timestamp NOT NULL,
   PRIMARY KEY (creator_id, platform_id),
-  FOREIGN KEY (creator_id) REFERENCES creators (id),
-  FOREIGN KEY (platform_id) REFERENCES platforms (id)
+  FOREIGN KEY (creator_id) REFERENCES creators(id),
+  FOREIGN KEY (platform_id) REFERENCES platforms(id)
 );
 
-CREATE TABLE creator_demographics (
+CREATE TABLE creator_demographic (
   creator_id integer,
-  demographics_id integer,
-  demographic varchar(255),
+  demographic_id integer,
+  demo varchar(255),
   last_update timestamp NOT NULL,
-  PRIMARY KEY (creator_id, demographics_id),
-  FOREIGN KEY (creator_id) REFERENCES creators (id),
-  FOREIGN KEY (demographics_id) REFERENCES demographics (id)
+  PRIMARY KEY (creator_id, demographic_id),
+  FOREIGN KEY (creator_id) REFERENCES creator(id),
+  FOREIGN KEY (demographic_id) REFERENCES demographic(id)
 );
