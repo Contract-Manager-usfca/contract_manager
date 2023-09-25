9cf03493ea02a56860ae5d92f0ee1a8c19de7bad
MalekeHan <malekehanson@gmail.com>
2023-09-24 21:57:18 -0700

    Setting up Django-DB connection

---
+++ contract_manager/contract_manager_application/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ContractManagerApplicationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'contract_manager_application'
