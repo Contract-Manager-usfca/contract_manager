28282814fa3c7dccc67d8947556e51fd2a822538
MalekeHan <Malekehanson@gmail.com>
2023-09-30 21:00:23 -0700

    CAN CONNECT TO SERVER THROUGH POSTMAN

--- contract_manager/settings.py
+++ contract_manager/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-b@muh$za!n%b#kmh0*com17-gd_1*a3(4-oi*%&1g7j5uwhh5#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'host.docker.internal']
+
 
 
 # Application definition
@@ -99,7 +100,7 @@ DATABASES = {
         'NAME': 'user040',
         'USER': 'user040',
         'PASSWORD': 'user040',
-        'HOST': '127.0.0.1',
+        'HOST': 'host.docker.internal',
         'PORT': '3307',
     }
 }
