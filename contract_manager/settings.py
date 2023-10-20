34b1987a2c65c2f949f4fbc557e4312dc71b6ad8
MalekeHan <Malekehanson@gmail.com>
2023-10-19 17:53:45 -0700

    Testing SSL

--- contract_manager/settings.py
+++ contract_manager/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contract_manager_application',
+    'letsencrypt',
     'corsheaders',
 ]
 
--- frontend/contract-manager/src/components/HomePage.js
+++ frontend/contract-manager/src/components/HomePage.js
@@ -29,7 +29,7 @@ function HomePage() {
 
   const fetchAllUsers = () => {
     // should be url to connect to back --> currently wrong
-    axios.get('http://localhost:8000/creators/', { withCredentials: true })
+    axios.get('http://chupacabra.cs.usfca.edu:8000/creators/', { withCredentials: true })
       .then(response => {
         setUsers(response.data);
         console.log(response.data);
