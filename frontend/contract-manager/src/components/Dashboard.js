faa0e6fd56bf2518ac05aba917002743e58f84af
clarejw <cjwilson6@dons.usfca.edu>
2023-10-21 19:11:30 -0700

    .

--- frontend/contract-manager/src/components/Dashboard.js
+++ frontend/contract-manager/src/components/Dashboard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Dashboard() {
   return (
-    <div>
-      <h3> Dashboard </h3>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#252525', paddingBottom: '100px' }}>
+      <h3 style={{backgroundColor: '#C188FB'}}> Welcome </h3>
     </div>
   );
 }
